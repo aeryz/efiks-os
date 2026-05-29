50d0b66569970a2c5fc33dadb4af352e75274123
kernelnomad <kernelnomad@proton.me>
2026-05-29 18:59:53 +0300

    add further clarifications to the pool
    Signed-off-by: kernelnomad <kernelnomad@proton.me>

--- kernel/src/task/table.rs
+++ kernel/src/task/table.rs
@@ -13,6 +13,20 @@ static TASK_POOL: TaskPool = TaskPool(SpinLock::new(TaskPoolInner {
     pid_to_idx: BTreeMap::new(),
 }));
 
+/// A task pool that guarantees to have stable pointers to tasks meaning
+/// regardless of how many tasks are inserted or removed, all of the existing
+/// tasks will stay under the same address.
+///
+/// The fact that this is a pool instead of a contiguous table is because we
+/// don't have to preallocate a very large task array but instead, allocate
+/// chunks of memory as we need.
+///
+/// However, we should be very careful about removing a task from the pool
+/// because we hand over a raw pointers to tasks. If we remove any task while
+/// it's still being referenced to from some place in the kernel, any further
+/// accesses to it will be UB. Hence, keeping the accesses to tasks with raw
+/// pointers should be only done in known places such as the scheduler. We can
+/// still access to a task using the Pid in other places.
 struct TaskPool(SpinLock<TaskPoolInner>);
 
 struct TaskPoolInner {
@@ -20,6 +34,8 @@ struct TaskPoolInner {
     pid_to_idx: BTreeMap<Pid, (usize, usize)>,
 }
 
+/// Adds a task to the task pool and returns a pointer to it. As long as the
+/// task itself is not reaped, the pointer will be valid.
 pub fn add_task(task: Task) -> NonNull<Task> {
     let mut pool = TASK_POOL.0.lock();
 
