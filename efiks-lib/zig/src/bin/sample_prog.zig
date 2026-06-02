201f6806c3d501f727ac968e33f2940363cd59f0
kernelnomad <kernelnomad@proton.me>
2026-06-03 00:33:36 +0300

    use read at the userspace
    Signed-off-by: kernelnomad <kernelnomad@proton.me>

--- efiks-lib/zig/src/bin/sample_prog.zig
+++ efiks-lib/zig/src/bin/sample_prog.zig
@@ -2,7 +2,15 @@ const efiks = @import("efiks");
 
 export fn _start() noreturn {
     while (true) {
-        _ = efiks.write("hello world");
-        efiks.syscall_sleep_ms(1200);
+        var buf: [1]u8 = undefined;
+        const n_read = efiks.read(&buf);
+        if (n_read == 0) {
+            _ = efiks.write("\n");
+        } else if (n_read > 0) {
+            switch (buf[0]) {
+                127, 8 => _ = efiks.write("\x08 \x08"),
+                else => _ = efiks.write(@constCast(&buf)),
+            }
+        }
     }
 }
