8186f575eb1834a3b727201a9774f1258255254a
kernelnomad <kernelnomad@proton.me>
2026-06-03 00:32:23 +0300

    reenable interrupts after console print
    Signed-off-by: kernelnomad <kernelnomad@proton.me>

--- kernel/src/debug.rs
+++ kernel/src/debug.rs
@@ -3,11 +3,16 @@ use ksync::SpinLock;
 static CONSOLE_LOCK: SpinLock<()> = SpinLock::new(());
 
 pub fn printk<T: AsRef<[u8]>>(b: T) {
-    // TODO(aeryz): THIS IS GONNA 100% DEADLOCK UNLESS WE MAKE SURE THE INTERRUPTS
-    // ARE DISABLED. maybe a interrupt disabling spinlock where calling `lock`
-    // always disables interrupts first?
+    let sstatus = riscv::registers::Sstatus::read().raw();
+    riscv::registers::Sstatus::new(sstatus)
+        .disable_supervisor_interrupts()
+        .write();
+
     let _console_guard = CONSOLE_LOCK.lock();
     b.as_ref()
         .into_iter()
         .for_each(|b| riscv::sbi::console_putchar(*b));
+
+    drop(_console_guard);
+    riscv::registers::Sstatus::new(sstatus).write();
 }
