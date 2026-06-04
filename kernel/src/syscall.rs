0266210446aced164bbf083b99600d534ba5a480
kernelnomad <kernelnomad@proton.me>
2026-06-04 23:51:54 +0300

    add spawn syscall
    Signed-off-by: kernelnomad <kernelnomad@proton.me>

--- kernel/src/syscall.rs
+++ kernel/src/syscall.rs
@@ -92,7 +92,55 @@ pub fn dispatch_syscall(tf: &mut TrapFrameOf<Arch>) {
             let time_ms = tf.get_arg::<0>();
             sched::sleep_current_task(time_ms);
         }
-        Syscall::Spawn => {}
+        Syscall::Spawn => {
+            let pid_ptr = tf.get_arg::<0>() as *mut task::Pid;
+            let path_ptr = tf.get_arg::<1>() as *const u8;
+
+            if pid_ptr == ptr::null_mut() || path_ptr == ptr::null() {
+                tf.set_syscall_return_value(usize::MAX);
+                return;
+            }
+
+            let path = unsafe {
+                let mut count = 0;
+                loop {
+                    if count >= vfs::MAX_FILE_PATH_LENGTH {
+                        tf.set_syscall_return_value(usize::MAX);
+                        return;
+                    }
+
+                    if *(path_ptr.offset(count as isize)) == 0 {
+                        break core::slice::from_raw_parts(path_ptr, count);
+                    }
+
+                    count += 1;
+                }
+            };
+
+            let this_ctx = unsafe {
+                Arch::load_this_cpu_ctx::<percpu::PerCoreContext>()
+                    .as_mut()
+                    .unwrap()
+            };
+
+            unsafe {
+                *pid_ptr = match task::spawn(path, Some(this_ctx.currently_running_task)) {
+                    Ok(pid) => pid,
+                    Err(_) => {
+                        tf.set_syscall_return_value(usize::MAX);
+                        return;
+                    }
+                };
+            }
+
+            unsafe {
+                this_ctx.currently_running_task.as_mut().state = task::TaskState::Blocked;
+            }
+
+            tf.set_syscall_return_value(0);
+
+            sched::schedule();
+        }
         _ => unreachable!(),
     }
 }
