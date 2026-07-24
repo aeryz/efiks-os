37b5417ffcecc6fe85caaab60272b284dd10bfc9
kernelnomad <kernelnomad@proton.me>
2026-07-24 22:19:47 +0300

    userspace: reimpl spawned_prog for linux
    Signed-off-by: kernelnomad <kernelnomad@proton.me>

--- userspace/zig/shell/src/bin/spawned_prog.zig
+++ userspace/zig/shell/src/bin/spawned_prog.zig
@@ -1,58 +1,17 @@
 const efiks = @import("efiks");
 const std = @import("std");
 
-pub const std_options = std.Options{
-    .page_size_max = 4096,
+pub const std_options: std.Options = .{
+    .signal_stack_size = null,
+    .enable_segfault_handler = false,
 };
 
-export fn _start() callconv(.naked) noreturn {
-    asm volatile (
-        \\  mv a0, sp
-        \\  tail __efiks_start
-        ::: .{ .memory = true });
-}
+pub fn main(_: std.process.Init.Minimal) !void {
+    var buffer: [256]u8 = undefined;
+    var writer: std.Io.Writer = .fixed(&buffer);
 
-export fn __efiks_start(sp: usize) callconv(.c) noreturn {
-    const argc_ptr: *const usize = @ptrFromInt(sp);
-    const argc = argc_ptr.*;
+    try writer.print("hello world from the spawned program\n", .{});
+    try writer.flush();
 
-    const argv: [*]const ?[*:0]const u8 = @ptrFromInt(sp + @sizeOf(usize));
-
-    const exit_code = main(argc, argv) catch -1;
-
-    efiks.syscall_exit(exit_code);
-
-    while (true) {}
-}
-
-const brk_allocator: std.mem.Allocator = .{
-    .ptr = undefined,
-    .vtable = &efiks.BrkAllocator.vtable,
-};
-
-fn main(argc: usize, argv: [*]const ?[*:0]const u8) !i32 {
-    var arena = std.heap.ArenaAllocator.init(brk_allocator);
-    var allocator = arena.allocator();
-    const ptr = try allocator.create(usize);
-    ptr.* = argc;
-
-    var buf: [64]u8 = undefined;
-    const s = try std.fmt.bufPrint(&buf, "Ptr is {} {} {}?", .{ argc, ptr.*, ptr });
-    _ = efiks.write(@constCast(s));
-
-    var i: usize = 0;
-    while (argv[i]) |arg| : (i += 1) {
-        _ = efiks.syscall_write(arg, strlen(arg));
-    }
-
-    _ = efiks.write("hello world from the spawned program\n");
     efiks.syscall_sleep_ms(2000);
-
-    return 0;
-}
-
-fn strlen(s: [*:0]const u8) usize {
-    var len: usize = 0;
-    while (s[len] != 0) : (len += 1) {}
-    return len;
 }
